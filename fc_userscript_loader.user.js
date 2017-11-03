b7ae937af373b75a4d6780bfe8808f0db210a5bb
Logan Stuart <fishpen0@users.noreply.github.com>
2017-11-03 16:12:47 -0400

    Update fc_userscript_loader.user.js

--- fc_userscript_loader.user.js
+++ fc_userscript_loader.user.js
@@ -1,21 +1,21 @@
 // ==UserScript==
 // @name           Frozen Cookies
 // @version        github-latest
-// @description    Userscript to load Frozen Cookies written by Icehawk78, forked by Lordshinjo
-// @author         Lordshinjo
-// @homepage       https://github.com/Lordshinjo/FrozenCookies
+// @description    Userscript to load Frozen Cookies written by Icehawk78, forked by Lordshinjo, forked by fishpen0
+// @author         fishpen0
+// @homepage       https://github.com/fishpen0/FrozenCookies
 // @include        http://orteil.dashnet.org/cookieclicker/
-// @updateURL      https://rawgit.com/Lordshinjo/FrozenCookies/master/fc_userscript_loader.js
-// @downloadURL    https://rawgit.com/Lordshinjo/FrozenCookies/master/fc_userscript_loader.js
+// @updateURL      https://rawgit.com/fishpen0/FrozenCookies/master/fc_userscript_loader.js
+// @downloadURL    https://rawgit.com/fishpen0/FrozenCookies/master/fc_userscript_loader.js
 // @run-at         document-start
 // ==/UserScript==
 
-// Dev:       https://github.com/Lordshinjo/FrozenCookies/development/
-// Master:    https://github.com/Lordshinjo/FrozenCookies/master/
-// Github.io: http://lordshinjo.github.io/FrozenCookies/
+// Dev:       https://github.com/fishpen0/FrozenCookies/development/
+// Master:    https://github.com/fishpen0/FrozenCookies/master/
+// Github.io: http://fishpen0.github.io/FrozenCookies/
 
 function LoadFrozenCookies() {
-    Game.LoadMod('https://rawgit.com/Lordshinjo/FrozenCookies/master/frozen_cookies.js');
+    Game.LoadMod('https://rawgit.com/fishpen0/FrozenCookies/master/frozen_cookies.js');
 }
 
 window.addEventListener("load", LoadFrozenCookies, false);
